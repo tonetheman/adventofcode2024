9c11d960f30f905a7f7c3768c824cbf611031f9d
tony(kitchen) <tonetheman@gmail.com>
2024-12-02 10:41:25 -0600

    recheck mostly works need move stuff around

--- day2/day2.py
+++ day2/day2.py
@@ -39,10 +39,9 @@ def test1():
 
 def check_single_row(row):
     final_res = False
-
     al3_counts = {}
     updown_counts = {False:0,True:0}
-    print(len(row),row)
+    # print(len(row),row)
     for i in range(len(row)-1):
         res = atLeastOneMostThree(row[i],row[i+1])
         if res in al3_counts:
@@ -52,21 +51,20 @@ def check_single_row(row):
         res = row[i]<row[i+1]
         updown_counts[res] += 1
 
-        print(row[i],row[i+1],atLeastOneMostThree(row[i],row[i+1]), row[i]<row[i+1])
-    print("al3 counts",al3_counts)
-    print("updown",updown_counts)
+        # print(row[i],row[i+1],atLeastOneMostThree(row[i],row[i+1]), row[i]<row[i+1])
+    # print("al3 counts",al3_counts)
+    # print("updown",updown_counts)
     if al3_counts[True] == len(row)-1:
         # they have enough trues to need second comparison
         if updown_counts[False]==len(row)-1 or updown_counts[True]==len(row)-1:
-            print("SUCCESS")
+            # print("SUCCESS")
             final_res = True
         else:
-            print("FAIL up down counts")
+            # print("FAIL up down counts")
             final_res = False
     else:
-        print("FAIL for at least one and most 3")
+        # print("FAIL for at least one and most 3")
         final_res = False
-    print()
     return final_res
 
 def part1():
@@ -81,7 +79,65 @@ def part1():
 
     print(good_count)
 
-def part2():
-    pass
+def recheck(row):
+    """
+    the row as it came failed
+    now remove single items and recheck each of them
+    """
+    orig = []
+    for i in row:
+        orig.append(i)
+    
+    print("ORIG",orig)
 
-part1()
+
+def make_new_skip_index(row,index):
+    tmp = []
+    for i in range(len(row)):
+        if i==index:
+            pass
+        else:
+            tmp.append(row[i])
+    return tmp
+
+def test_recheck():
+    # will not be good if we remove at least 1
+    row = [1,2,7,8,9]
+    
+    # will be good if we remove at least 1
+    row = [1,3,2,4,5]
+        
+    res = check_single_row(row)
+    print("check single row", res)
+    count = 0
+    for i in range(len(row)):
+        # make a new row skipping the index i
+        new_row = make_new_skip_index(row,i)
+        print("new_row",new_row)
+        new_res = check_single_row(new_row)
+        print("new res",new_res)    
+        if new_res: count += 1
+    
+    if count>0:
+        print("WOULD BE GOOD WITH DAMP")
+    else:
+        print("FAILED DAMP")
+
+
+
+
+def part2():
+    good_count = 0
+    for row in tmp_data:
+        res = check_single_row(row)
+        if res:
+            good_count += 1
+        else:
+            # now we need to remove things and retest!
+            recheck(row)
+            print("WOULD REMOVE AND RETEST")
+            print()
+
+# part1()
+# part2()
+test_recheck()
