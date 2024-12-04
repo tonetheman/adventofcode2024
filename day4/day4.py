ba70aaeda51dab70ea87f15e2a4ff85feb0c2042
tony(kitchen) <tonetheman@gmail.com>
2024-12-04 11:53:10 -0600

    splitting data up and adding padding

--- day4/day4.py
+++ day4/day4.py
@@ -1,6 +1,27 @@
 
-filename = "sample.txt"
-data = open(filename,"r").readlines()
-data = [line.strip() for line in data]
 
-print(data)
+
+def get_data():
+    filename = "sample.txt"
+    data = open(filename,"r").readlines()
+    data = [line.strip() for line in data]
+    return data
+
+def add_padding(data):
+    # adding padding to make my life easier
+    d=[]
+
+    # if you call list() on a string you get an array of chars!
+    d.append(list("."*(2+len(data[0]))))
+
+    for line in data:
+        new_line = "." + line + "."
+        d.append(list(new_line))
+    
+    d.append(list("."*(2+len(data[0]))))
+    
+    return d
+
+data = get_data()
+new_data = add_padding(data)
+print(new_data)
