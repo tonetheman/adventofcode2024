f72086ad2ef598280dd927a26f8e4a947a4f3df9
tonethemna <tonetheman@gmail.com>
2024-12-26 11:59:34 -0600

    saving dumb start

--- day5/prog.py
+++ day5/prog.py
@@ -22,7 +22,7 @@ def get_data():
         try:
             line = lines[index]
         except IndexError:
-            print("end of lines",index)
+            # print("end of lines",index)
             break
         
         m = P1.match(line)
@@ -40,10 +40,24 @@ def get_data():
 
         tmp = line.split(",")
         # TODO save updates here
-        res.orders.append(tmp)    
+        res.orders.append(list(map(int,tmp)))    
         index += 1
     return res
 
+def get_rules_for_order(d:DataHolder,c_order):
+    # this really should be a map
+    # do not need duplicate rules
+    # in this list I think
+    res = []
+    for val in c_order:
+        for rule in d.rules:
+            if rule[0]==val or rule[1]==val:
+                res.append(rule)
+    return res
+        
 
 d = get_data()
-print(d)
+c_order = d.orders[0]
+print("current order",c_order)
+rules_to_check = get_rules_for_order(d,c_order)
+print("rules to check",rules_to_check)
